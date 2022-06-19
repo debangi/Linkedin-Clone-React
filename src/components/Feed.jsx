313b1bb45b9015ed12bcd4959876cb46c9546c18
debangi <debangiofficial@gmail.com>
2022-06-19 19:04:20 +0530

    Creating post component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CreateIcon from '@mui/icons-material/Create';
 import ImageIcon from '@mui/icons-material/Image';
 import EventNoteIcon from '@mui/icons-material/EventNote';
@@ -6,8 +6,15 @@ import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import ArticleIcon from '@mui/icons-material/Article';
 import './Feed.css';
 import InputOption from './InputOption';
+import Post from './Post';
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='feed'>
       <div className='feed__inputContainer'>
@@ -15,7 +22,9 @@ const Feed = () => {
           <CreateIcon />
           <form>
             <input type='text' />
-            <button type='submit'>Send</button>
+            <button onClick={sendPost} type='submit'>
+              Send
+            </button>
           </form>
         </div>
         <div className='feed__inputOptions'>
@@ -29,6 +38,20 @@ const Feed = () => {
           />
         </div>
       </div>
+      {posts.map((post) => (
+        <Post
+          name='debangi'
+          description='test'
+          message='jjkjsjsdidiu'
+          photoUrl=''
+        />
+      ))}
+      <Post
+        name='debangi'
+        description='test'
+        message='jjkjsjsdidiu'
+        photoUrl=''
+      />
     </div>
   );
 };
